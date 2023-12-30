8887251780507ff667ca979c4f951da66002dcc0
Emmanuel  Trejo <115524511+Trexpapu@users.noreply.github.com>
2023-12-29 20:39:17 -0600

    Update auto.py

--- auto.py
+++ auto.py
@@ -3,11 +3,12 @@ import webbrowser, pyautogui
 import time as t
 import pygame as p
 from tkinter import messagebox
+import speedtest 
 conexion = d.conexion
 cursor = d.cursor
 def eliminarUsuariosSinMensaje():
     try:
-       # print("xd")
+
         # Ejecuta la sentencia SQL para eliminar usuarios sin mensajes
         cursor.execute("DELETE FROM USUARIOS WHERE ID NOT IN (SELECT DISTINCT PERSONA_ID FROM MENSAJES)")
         
@@ -29,36 +30,51 @@ def enviar_mensajes():
     sound = p.mixer.Sound('sonido.mp3')
 
     estructura_tiempo = t.localtime(t.time())
+
+    st = speedtest.Speedtest()
+    vel_descarga = st.download() / 10**6  # Convertir a megabits por segundo
+    tiempo = 60
+    if vel_descarga <=10:
+        tiempo = 90
+    elif vel_descarga <=20:
+        tiempo = 50
+    elif vel_descarga <= 50:
+        tiempo = 28
+    elif vel_descarga <= 100:
+        tiempo = 22
+    else:
+        tiempo = 15
+   # print(f"Velocidad de descarga: {vel_descarga:.2f} Mbps")
+   # print(tiempo)
+
     try:
-        cursor.execute("SELECT * FROM USUARIOS")
-        usuarios = cursor.fetchall()
         cursor.execute("SELECT * FROM MENSAJES")
         mensajes = cursor.fetchall()
         fecha_actual = f"{estructura_tiempo.tm_year} {estructura_tiempo.tm_mon} {estructura_tiempo.tm_mday}"
-        indice = 0
         mensajes_encontrados = False
         webbrowser.open("https://web.whatsapp.com")
-        t.sleep(60)
+        t.sleep(tiempo)
 
         for mensaje in mensajes:
             fecha = f"{mensaje[2]} {mensaje[3]} {mensaje[4]}"
             
             if fecha_actual == fecha:
                 #igual
-                celular_destino =f"{usuarios[indice][1]}"
-                print(celular_destino, mensaje[5], mensaje[0])
+                cursor.execute("SELECT U.CELULAR FROM USUARIOS AS U WHERE ID = ?", (mensaje[1], ))
+                celular_destino = cursor.fetchone()
+                #celular_destino =f"{usuarios[indice][1]}"
+             #   print(celular_destino, mensaje[5], mensaje[0])
                 webbrowser.open("https://web.whatsapp.com/send?phone="+f"{celular_destino}")
-                t.sleep(30)
+                t.sleep(tiempo)
                 pyautogui.typewrite(mensaje[5])
-                t.sleep(3)
+                t.sleep(5)
                 pyautogui.press("enter")
-                t.sleep(10)
+                t.sleep(15)
                 id_ = mensaje[0]
                 cursor.execute("DELETE FROM MENSAJES WHERE ID = ?", (id_,))
                 conexion.commit()
                 mensajes_encontrados = True
-            indice += 1        
-            
+
         sound.play()
         p.time.wait(int(sound.get_length() * 1000))
         p.quit()
@@ -67,6 +83,7 @@ def enviar_mensajes():
 
 
     except Exception as e:
+        print(e)
         messagebox.showinfo("Error", "Ocurrio un error al enviar los mensajes")
     finally:
 
@@ -74,7 +91,3 @@ def enviar_mensajes():
             messagebox.showinfo("Error", "No hay mensajes por enviar hoy")
         elif mensajes_encontrados == True:
             messagebox.showinfo("Exito","Mensajes enviados")
-    
-
-
-
